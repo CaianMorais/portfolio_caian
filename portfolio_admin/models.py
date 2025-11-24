72e51ad7c770be297f9832c2b2f092d58c94e375
CaianMorais <caianqm@gmail.com>
2025-11-24 16:38:11 -0400

    commit 2

--- portfolio_admin/models.py
+++ portfolio_admin/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+def upload_projeto(instance, filename):
+    return f'midia/projeto-{instance.titulo}/{filename}'
+
+def upload_sobre(instance, filename):
+    return f'midia/sobre-{instance.id}/{filename}'
+
+def upload_tecnologia(instance, filename):
+    return f'midia/tecnologia-{instance.id}/{filename}'
+
+def upload_contato(instance, filename):
+    return f'midia/contato-{instance.id}/{filename}'
+
 # Create your models here.
 class Projetos(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=200, null=False, blank=False)
     subtitulo = models.CharField(max_length=300, null=False, blank=False)
     descricao = models.TextField(max_length=10000, null=False, blank=False)
-    midia = models.ImageField(upload_to=f'midia/projeto-{id}', null=True, blank=True)
+    midia = models.ImageField(upload_to=upload_projeto, null=True, blank=True)
     publico = models.BooleanField(default=True)
 
     def __str__(self):
@@ -17,7 +29,7 @@ class Sobre(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     profissao = models.CharField(max_length=100, null=False, blank=False)
     descricao = models.TextField(max_length=5000, null=False, blank=False)
-    foto = models.ImageField(upload_to=f'midia/sobre-{id}', null=True, blank=True)
+    foto = models.ImageField(upload_to=upload_sobre, null=True, blank=True)
     publico = models.BooleanField(default=True)
 
     def __str__(self):
@@ -26,7 +38,7 @@ class Sobre(models.Model):
 class Tecnologias(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100, null=False, blank=False)
-    icone = models.ImageField(upload_to=f'midia/tecnologia-{id}', null=True, blank=True)
+    icone = models.ImageField(upload_to=upload_tecnologia, null=True, blank=True)
     publico = models.BooleanField(default=True)
 
     def __str__(self):
@@ -36,7 +48,7 @@ class Contato(models.Model):
     id = models.AutoField(primary_key=True)
     tipo = models.CharField(max_length=100, null=False, blank=False)
     link = models.URLField(max_length=500, null=False, blank=False)
-    icone = models.ImageField(upload_to=f'midia/contato-{id}', null=True, blank=True)
+    icone = models.ImageField(upload_to=upload_contato, null=True, blank=True)
 
     def __str__(self):
         return self.tipo
