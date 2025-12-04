d9cad79c72b2c76462c62c3a6488f9a9bb05a5f3
CaianMorais <caianqm@gmail.com>
2025-12-04 08:46:15 -0400

    configuração para buildar app na vercel

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "Buildando o projeto..."
+
+pip install -r requirements.txt
+
+python3 manage.py makeigrations
+python3 manage.py migrate
+
+npm install
+npm run build:css
+
+echo "Build concluído!"
